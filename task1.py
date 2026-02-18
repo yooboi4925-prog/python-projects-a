13b708867f2400bde8ccca8747dcf863861df5d0
yooboi4925-prog <yooboi.4925@gmail.com>
2026-02-18 13:30:31 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+num=int(input('enter a number  : '))
+if num  % 2 == 0 :
+    print ( num , "is an even number ")
+else:
+    print ( num ,"is an odd number ")
+
+
+
+
---
+++ task2.py
@@ -0,0 +1,1 @@
+print(f"the sum of number from 1 to 50 {sum(range(1,51))}")
